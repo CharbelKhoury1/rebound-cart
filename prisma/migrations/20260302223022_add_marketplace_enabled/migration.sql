1c32b0dc226ea23fd4431ad986c1d9430d648b5f
CharbelKh <ckhoury100@gmail.com>
2026-03-03 00:31:29 +0200

    changes

---
+++ prisma/migrations/20260302223022_add_marketplace_enabled/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_ShopSettings" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "shop" TEXT NOT NULL,
+    "commissionRate" DECIMAL NOT NULL DEFAULT 10.0,
+    "isMarketplaceEnabled" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_ShopSettings" ("commissionRate", "createdAt", "id", "shop", "updatedAt") SELECT "commissionRate", "createdAt", "id", "shop", "updatedAt" FROM "ShopSettings";
+DROP TABLE "ShopSettings";
+ALTER TABLE "new_ShopSettings" RENAME TO "ShopSettings";
+CREATE UNIQUE INDEX "ShopSettings_shop_key" ON "ShopSettings"("shop");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
